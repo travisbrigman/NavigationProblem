91b9d178b750087a69f2e26447d84b04d8931843
Travis Brigman <travis.brigman@hcahealthcare.com>
2023-01-10 07:31:49 -0600

    Initial Commit

---
+++ NavigationProblem/NavigationProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationProblemApp.swift
+//  NavigationProblem
+//
+//  Created by Travis Brigman on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
